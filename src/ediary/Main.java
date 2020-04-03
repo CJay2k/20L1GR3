6c5339e2880c85e5dc0efbe5342a8a78454750f3
CJay2k <cjay2k12@gmail.com>
2020-04-03 18:17:38 +0200

    Change login window title

--- src/ediary/Main.java
+++ src/ediary/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxmls/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Logowanie e-Dziennik");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
