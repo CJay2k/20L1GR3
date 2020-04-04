d2c19a418a2a8cf58a6958856b236eed81529201
CJay2k <cjay2k12@gmail.com>
2020-04-04 14:35:02 +0200

    JavaFX error workaround

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,6 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxmls/login.fxml"));
-        primaryStage.setTitle("Logowanie e-Dziennik");
-        primaryStage.setScene(new Scene(root, 400, 300));
-        primaryStage.show();
-    }
-
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        MainFX.main(args);
     }
 }
