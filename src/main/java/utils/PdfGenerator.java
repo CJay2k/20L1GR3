2c546cdf5a8a3c85698cc4cf56a1e3e7a0337fb2
CJay2k <cjay2k12@gmail.com>
2020-06-09 02:36:15 +0200

    Cleanup code

--- src/main/java/utils/PdfGenerator.java
+++ src/main/java/utils/PdfGenerator.java
@@ -1,7 +1,5 @@
 package utils;
 
-import javafx.collections.ObservableList;
-import mapping.Oceny;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
@@ -9,24 +7,14 @@ import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import javafx.collections.ObservableList;
+import mapping.Oceny;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 
 public class PdfGenerator {
-    public void PDFGenerateGrades(ObservableList<Oceny> listaOcen) throws IOException, DocumentException {
-        Document doc = new Document();
-        PdfWriter.getInstance(doc, new FileOutputStream("raport.pdf"));
-        doc.open();
-
-
-        doc.add(new Paragraph("Wykaz ocen ucznia"));
-        doc.add(new Paragraph("\n"));
-        doc.add(createTable(listaOcen));
-        doc.close();
-    }
-
-
     private static PdfPTable createTable(ObservableList<Oceny> listaOcen) {
         PdfPTable table = new PdfPTable(4);
         PdfPCell cell;
@@ -43,9 +31,21 @@ public class PdfGenerator {
         for (int i = 0; i < listaOcen.size(); i++) {
             table.addCell(listaOcen.get(i).getData().toString());
             table.addCell(listaOcen.get(i).getPrzedmiotyByPrzedmiotId().toString());
-            table.addCell(listaOcen.get(i).getWartosc().toString());
-            table.addCell(listaOcen.get(i).getOpis().toString());
+            table.addCell(listaOcen.get(i).getWartosc());
+            table.addCell(listaOcen.get(i).getOpis());
         }
         return table;
+    }
+
+    public void PDFGenerateGrades(ObservableList<Oceny> listaOcen) throws IOException, DocumentException {
+        Document doc = new Document();
+        PdfWriter.getInstance(doc, new FileOutputStream("raport.pdf"));
+        doc.open();
+
+
+        doc.add(new Paragraph("Wykaz ocen ucznia"));
+        doc.add(new Paragraph("\n"));
+        doc.add(createTable(listaOcen));
+        doc.close();
     }
 }
