ade1b30a425e1c3ff24da2c88f9ce9a1c2641929
xxkuba3 <jakub.galuszka97@gmail.com>
2020-06-10 03:01:46 +0200

    dodanie komentarzy JavaDoc

--- src/main/java/controllers/SessionController.java
+++ src/main/java/controllers/SessionController.java
@@ -1,11 +1,27 @@
+/**
+ * Package controllers przechowuje klasy, odpowiadające za sterowanie naszą aplikacją.
+ */
+
 package controllers;
 
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+/**
+ * Klasa SessionController odpowiada za zarządzanie sesja w naszej aplikacji.
+ *
+ * @author Adrian Hrycaj, Jakub Gałuszka, Paweł Kolano, Mateusz Jedziniak, Aleksander Jewuła
+ * @version 1.0
+ * @since   2020-05-02
+ */
+
 public class SessionController {
+    /**
+     * Zmienna ourSessionFactory odnosi się do SessionFactory to klasa fabryczna, za pośrednictwem której uzyskujemy sesje i wykonujemy operacje na bazie danych.
+     */
     private static final SessionFactory ourSessionFactory;
+
 
     static {
         try {
@@ -18,6 +34,11 @@ public class SessionController {
         }
     }
 
+    /**
+     * Metoda getSession Służy do pobrania i otworzenia naszej sesji.
+     * @throws HibernateException Wyrzuca nam błąd, jeśli Hibernate ma problem z pobraniem naszej sesji.
+     * @return ourSessionFactory.openSession() Służy do załadowania naszej sesji.
+     */
     public static org.hibernate.Session getSession() throws HibernateException {
         return ourSessionFactory.openSession();
     }
