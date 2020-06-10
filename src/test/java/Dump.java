d979db7674f086b2c864aed8fea06ed34d5b5a8f
xxkuba3 <jakub.galuszka97@gmail.com>
2020-06-10 03:05:47 +0200

    Dodanie komenatrzy JavaDoc

--- src/test/java/Dump.java
+++ src/test/java/Dump.java
@@ -1,11 +1,29 @@
 import java.io.IOException;
 
+/**
+ * Klasa Dump odpowiada za wyczyszczenie i ponowne załadowanie danych do bazy.
+ *
+ * @author Adrian Hrycaj, Jakub Gałuszka, Paweł Kolano, Mateusz Jedziniak, Aleksander Jewuła
+ * @version 1.0
+ * @since   2020-05-02
+ *
+ */
 public class Dump {
 
+    /**
+     * Metoda main jest to główna metoda naszej klasy.
+     * @param args Parametr przechowujący liste argumentów.
+     * @throws Exception Wyrzuci nam kiedy będą błędne dane do logowania w bazie.
+     */
     public static void main(final String[] args) throws Exception {
         loadDump();
     }
 
+    /**
+     * Metoda loadDump służy do czyszczenia i uzupełniania bazy danych.
+     * @throws IOException Wystąpi wówczas, gdy nie znajdzie pliku, bądź nie będzie miało dostępu.
+     * @throws InterruptedException Wystąpi wówczas, gdy proces ładowania zostanie przerwany.
+     */
     public static void loadDump() throws IOException, InterruptedException {
 
         System.out.println("Czyszczenie bazy danych...");
