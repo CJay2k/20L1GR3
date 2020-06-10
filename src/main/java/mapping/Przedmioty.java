1967403db878bc445447beca13ff58d77edbd4f7
xxkuba3 <jakub.galuszka97@gmail.com>
2020-06-10 03:03:37 +0200

    Dodanie komenatrzy JavaDoc

--- src/main/java/mapping/Przedmioty.java
+++ src/main/java/mapping/Przedmioty.java
@@ -1,17 +1,49 @@
+/**
+ * Package mapping przechowuje obiekty, ktore generuje Hibernate na podstawie naszej bazy danych
+ */
 package mapping;
 
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Objects;
 
+/**
+ * Klasa Przedmioty, przechowuje informacje o przedmiotach wykładanych w szkole.
+ *
+ * @author Adrian Hrycaj, Jakub Gałuszka, Paweł Kolano, Mateusz Jedziniak, Aleksander Jewuła
+ * @version 1.0
+ * @since   2020-05-11
+ */
+
 @Entity
 public class Przedmioty {
+    /**
+     * Zmienna przechowująca klucz główny 'id' naszej klasy Przedmioty.
+     */
     private int przedmiotId;
+    /**
+     * Zmienna przechowująca nazwe przedmiotu.
+     */
     private String nazwaPrzedmiotu;
+    /**
+     * Zmienna przechowująca informacje o klasie, w której wykładany jest dany przedmiot.
+     */
     private Collection<KlasyPrzedmioty> klasyPrzedmiotiesByPrzedmiotId;
+    /**
+     * Zmienna przechowująca informacje o nieobecnościach na danych przedmiocie.
+     */
     private Collection<Nieobecnosci> nieobecnoscisByPrzedmiotId;
+    /**
+     * Zmienna przechowująca informacje o ocenach z danego przedmiotu.
+     */
     private Collection<Oceny> oceniesByPrzedmiotId;
+    /**
+     * Zmienna przechowująca informacje o nauczycielu wykładacy dane przedmioty.
+     */
     private Nauczyciele nauczycieleByNauczycielId;
+    /**
+     * Zmienna przechowująca uwagi otrzymane na danym przedmiocie.
+     */
     private Collection<Uwagi> uwagisByPrzedmiotId;
 
     @Id
