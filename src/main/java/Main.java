b58249f23402c54d04c1718f4122a1a3e36daadc
xxkuba3 <jakub.galuszka97@gmail.com>
2020-06-10 02:57:54 +0200

    Dodanie komentarzy JavaDoc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+/**
+ * Klasa Main, czyli jest główna klasa naszej aplikacji służąca za uruchomienie jej.
+ *
+ * @author Adrian Hrycaj, Jakub Gałuszka, Paweł Kolano, Mateusz Jedziniak, Aleksander Jewuła
+ * @version 1.0
+ * @since   2020-04-03 *
+ *
+ */
+
 public class Main {
 
+    /**
+     * Główna metoda klasy Main, służąca do otwarcia klasy MainFX, czyli GUI naszej apliacji.
+     * @param args Zawiera dostarczone argumenty wiersza polecenia jako tablicę String obiektów.
+     */
     //    Project Main function
     public static void main(String[] args) {
 
